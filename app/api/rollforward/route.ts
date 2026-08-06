59c9ce9279b3fbf34955fda80d87eaa7b367c03a
Chuckshub <Charles@coder.com>
2026-08-05 20:45:02 -0600

    change

--- app/api/rollforward/route.ts
+++
@@ -1,9 +0,0 @@
-import { json, withUser } from "@/lib/api";
-import { loadEngineContracts } from "@/lib/data";
-import { computePortfolio } from "@/lib/engine";
-
-export const GET = withUser(async () => {
-  const inputs = await loadEngineContracts();
-  const { months, byContract } = computePortfolio(inputs);
-  return json({ months, byContract });
-});
